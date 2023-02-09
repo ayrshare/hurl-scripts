6f5cf3003555474ae6243ac653fe39d8332e8544
Shyam Vijayakumar <shyam@ayrshare.com>
2023-02-09 13:22:46 +0530

    Split up posts by network

--- script.bash
+++ script.bash
@@ -1,2 +1,2 @@
 export HURL_NOW=$(date '+%y%m%d%h%s')
-hurl --test --variables-file vars.env analytics_social.hurl user.hurl post_flow.hurl history.hurl history_get_all.hurl generate_jwt.hurl createProfile.hurl short.hurl autoschedule.hurl
+hurl --test --variables-file vars.env --retry --retry-interval 2000 --retry-max-count 3 analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl createProfile.hurl short.hurl autoschedule.hurl
