a30df66d8c52f99d9bfe8b3cbe597c45e904a274
Geoffrey Bourne <gbourne@gmail.com>
2023-02-14 18:03:08 -0500

    updated packages

---
+++ script-prod.bash
@@ -0,0 +1,2 @@
+export HURL_NOW=$(date '+%y%m%d%h%s')
+hurl --test --variables-file vars-prod.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_pi.hurl post_flow_tw.hurl post_flow_li.hurl history.hurl history_get_all.hurl generate_jwt.hurl createProfile.hurl short.hurl autoschedule.hurl
