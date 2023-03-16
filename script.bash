b4f4f1eb5b2b59b98a7fc7c28d4a01ac57d1d0b6
Shyam Vijayakumar <shyam@ayrshare.com>
2023-03-16 18:25:51 +0530

    Hurl tests for link shortener and resize endpoints

--- script.bash
+++ script.bash
@@ -1,2 +1,2 @@
 export HURL_NOW=$(date '+%y%m%d%h%s')
-hurl --test --very-verbose --variables-file vars.env --retry --retry-interval 30000 --retry-max-count 4 --json analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl createProfile.hurl short.hurl autoschedule.hurl
+hurl --test --very-verbose --variables-file vars.env --retry --retry-interval 30000 --retry-max-count 4 --json analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl createProfile.hurl short.hurl autoschedule.hurl linkshort.hurl analytics_link.hurl resize.hurl
