c9fb942b908ff1f3b43a3f4e534b384fcdc562d4
Geoffrey Bourne <gbourne@gmail.com>
2023-09-21 10:29:01 -0400

    Added --error-format long

--- script-dev.bash
+++ script-dev.bash
@@ -1,2 +1,2 @@
 export HURL_NOW=$(date '+%y%m%d%h%s')
-hurl --test --very-verbose --report-html html --variables-file vars-dev.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl autoschedule.hurl resize.hurl feed.hurl hook.hurl profile.hurl autohashtag.hurl
+hurl --test --error-format long --report-html html --color --variables-file vars-dev.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl autoschedule.hurl resize.hurl feed.hurl hook.hurl profile.hurl autohashtag.hurl
