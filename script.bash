5269edd3508d28df67d984fd374bb55bad8dbce9
Geoffrey Bourne <gbourne1@users.noreply.github.com>
2023-12-08 15:36:07 -0500

    Updated prod scripts

--- script.bash
+++ script.bash
@@ -1,5 +1,5 @@
 rm -rf html
 rm -rf ../probe/hurl
 export HURL_NOW=$(date '+%y%m%d%h%s')
-hurl --test --error-format long --report-html html --variables-file vars.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_tt.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl autoschedule.hurl links.hurl resize.hurl feed.hurl hook.hurl profile.hurl autohashtag.hurl
+hurl --test --error-format long --report-html html --variables-file vars.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl autoschedule.hurl links.hurl resize.hurl feed.hurl hook.hurl profile.hurl autohashtag.hurl
 rsync -a html/* ../probe/hurl
