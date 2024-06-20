bb26b8524272de2b71fad233621474f93510ff9e
Geoffrey Bourne <gbourne1@users.noreply.github.com>
2024-06-20 08:14:52 -0400

    Removed feeds

--- script-prod.bash
+++ script-prod.bash
@@ -1,2 +1,2 @@
 export HURL_NOW=$(date '+%y%m%d%h%s')
-hurl --test --error-format long --report-html html --variables-file vars-prod.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl autoschedule.hurl links.hurl resize.hurl feed.hurl hook.hurl profile.hurl autohashtag.hurl messages.hurl
+hurl --test --error-format long --report-html html --variables-file vars-prod.env analytics_social.hurl user.hurl post_flow_fb.hurl post_flow_ig.hurl post_flow_li.hurl post_flow_pi.hurl post_flow_tw.hurl history.hurl history_get_all.hurl generate_jwt.hurl autoschedule.hurl links.hurl resize.hurl hook.hurl profile.hurl autohashtag.hurl messages.hurl
